1f29ab35860e7da18b052163b68fbed020b8bd91
Srikanth Mergu <srikanth.mergu@pegs.com>
2020-05-01 11:24:01 +0530

    Initial commit

---
+++ src/test/java/com/interview/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
