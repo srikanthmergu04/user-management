ae61cf44a759481060ee6752b786bb700d80d413
Srikanth Mergu <srikanth.mergu@pegs.com>
2020-05-01 11:41:38 +0530

    created user model.

---
+++ src/main/java/com/interview/usermanagement/model/User.java
@@ -0,0 +1,114 @@
+package com.interview.usermanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long uid;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private String gender;
+	@Column
+	private int age;
+	@Column
+	private String location;
+	@Column
+	private String phNum;
+	@Column
+	private String email;
+	@Column
+	private String userName;
+	@Column
+	private String password;
+
+	public long getUid() {
+		return uid;
+	}
+
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getPhNum() {
+		return phNum;
+	}
+
+	public void setPhNum(String phNum) {
+		this.phNum = phNum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
